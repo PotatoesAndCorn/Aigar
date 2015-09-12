ef41b6d3adcd976ce2476fb1f0f9767a7378784a
Gabe <gabe1118@yahoo.com>
2015-09-12 19:27:00 -0400

    collisions editing

--- engine.js
+++ engine.js
@@ -1,8 +1,14 @@
 var objectslib = require('./objects.js');
+
+var Vec2 = require('vec2');
+var Quadtree2 = require('quadtree2');
+
 var BlobObject = objectsLib.BlobObject;
 var CellObject = objectsLib.CellObject;
 
-function World(x = 1000, y = 1000) {
+function World(x, y) {
+    if(x == null){x = 1000;}
+    if(y == null){y = 1000;}
     this.foods = new Array(); //  array of BlobObjects
     this.players = new Array(); //  array of CellObjects
     this.x_max = x / 2;
@@ -11,7 +17,7 @@ function World(x = 1000, y = 1000) {
     this.y_min = -y / 2;
 
     this.getCollisions = function() {
-
+        return getCollisionsFromQuad(x,y, foods, players);
     };
 
     this.updatePositions = function() {
@@ -23,4 +29,56 @@ function World(x = 1000, y = 1000) {
     }
 }
 
+function getCollisionsFromQuad(x, y, foods, players)
+{   
+    // This will initialize a quadtree with a x*y resolution, 
+    // with an object limit of 3 inside a quadrant. 
+    quadtree = new Quadtree2(new Vec2(x, y), 3),
+    
+    itemstoadd = new Array();
+    for(entity in foods)
+    {
+        itemstoadd.push(
+            {
+                pos_: new Vec2(entity.position.x, entity.position.y),
+                rad_: entity.size*entity.size
+            });
+    }
+   
+    // Add all of our beloved character to the quadtree. 
+    quadtree.addObjects(itemstoadd);
+    pairs = new Array();
+    for(item in itemstoadd)
+    {
+        coll = quadtree.getCollisionsForObjects(item);
+        console.log(coll);
+        //pairs.push({a:coll.pos_, b:_id2});
+    }
+}
+
 module.exports.World = World;
+//create some characters 
+
+World.food.push(new BlobObject(10, new Vector2(500,650)));
+World.food.push(new BlobObject(50, new Vector2(500,650)));
+World.food.push(new BlobObject(20, new Vector2(300,567)));
+World.food.push(new BlobObject(15, new Vector2(700,780)));
+World.food.push(new BlobObject(12, new Vector2(230,230)));
+World.food.push(new BlobObject(7, new Vector2(540,523)));
+World.food.push(new BlobObject(8, new Vector2(450,300)));
+World.food.push(new BlobObject(12, new Vector2(345,550)));
+
+console.log(World.getCollisions());
+
+
+
+
+
+
+
+
+
+
+
+
+
